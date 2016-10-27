f50455617c61f0ab60204b5c9fe48d6ce852bf3a
ivanalbizu <ivan.albizu@gmail.com>
2016-10-27 11:22:37 +0200

    Minor fixes

--- footer.php
+++ footer.php
@@ -14,26 +14,24 @@
 php */ ?>
 			<footer class="page-footer cyan darken-3">
 			  <div class="container">
-				<div class="row">
-				  <div class="col l6 s12">
-					<h5 class="white-text">Conócenos</h5>
-					<p class="grey-text text-lighten-4">
-						<?php the_social_menu(); ?>
-					</p>
-				  </div>
-				  <div ng-controller="FooterCtrl as vm" class="col l4 offset-l2 s12">
-					<h5 class="white-text">Últimas viviendas en venta</h5>
-					<ul ng-if="vm.restApiActive && vm.acfActive">
-						<li ng-repeat="item in vm.posts"><a class="grey-text text-lighten-3" href="wordpress/#/comprar-vivienda/{{item.id}}">{{item.title.rendered}} - {{item.fields.venta_precio | currency:"€":0}}</a></li>
-					</ul>
-				  </div>
-				</div>
+					<div class="row">
+					  <div class="col m7 s12">
+							<h5 class="white-text">Conócenos</h5>
+							<p class="grey-text text-lighten-4"><?php the_social_menu(); ?></p>
+					  </div>
+					  <div ng-controller="FooterCtrl as vm" class="col m5 s12">
+							<h5 class="white-text">Últimas viviendas en venta</h5>
+							<ul ng-if="vm.restApiActive && vm.acfActive">
+								<li ng-repeat="item in vm.posts"><a class="grey-text text-lighten-3" href="#/comprar-vivienda/{{item.id}}">{{item.title.rendered}} - {{item.fields.venta_precio | currency:"€":0}}</a></li>
+							</ul>
+					  </div>
+					</div>
 			  </div>
 			  <div class="footer-copyright">
-				<div class="container">
-				<?php echo date("Y"); ?> &copy; Copyright
-				<a class="grey-text text-lighten-4 right" href="http://ivanalbizu.eu/">Maquetaci&oacute;n &amp; Programaci&oacute;n Iv&aacute;n Albizu</a>
-				</div>
+					<div class="container">
+						<span><?php bloginfo( 'name' ); ?> &copy; <?php echo date("Y"); ?> Copyright</span>
+						<a class="grey-text text-lighten-4 right" href="http://ivanalbizu.eu/">Maquetaci&oacute;n &amp; Programaci&oacute;n Iv&aacute;n Albizu</a>
+					</div>
 			  </div>
 			</footer>
 
